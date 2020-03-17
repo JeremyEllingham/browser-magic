6b89f382c925a672c3c7dc87b21fc162190f24d3
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-17 23:34:11 +0000

    Updates

---
+++ precache-manifest.f30aebcbcc3e5335d2f5305d6d6fcff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8feca94c6eb7214e78a88cc6353392",
+    "url": "/browser-magic/index.html"
+  },
+  {
+    "revision": "840bdc13648d74702c53",
+    "url": "/browser-magic/static/css/main.f9031d64.chunk.css"
+  },
+  {
+    "revision": "06829d752a19ac78114b",
+    "url": "/browser-magic/static/js/2.c64e1fad.chunk.js"
+  },
+  {
+    "revision": "fc546b2ab28544fdca15f51b30e06cdf",
+    "url": "/browser-magic/static/js/2.c64e1fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840bdc13648d74702c53",
+    "url": "/browser-magic/static/js/main.9fed3439.chunk.js"
+  },
+  {
+    "revision": "d3aad75cd4f7083e17f3",
+    "url": "/browser-magic/static/js/runtime-main.b65ce421.js"
+  }
+]);
