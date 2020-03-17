c39c62977538d4492dadbee44860f2a549188b98
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-17 23:12:20 +0000

    Set up grid.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <p>Text goes here.</p>
+    <div className="grid">
+      <div className="box-left-top"><p>Player 1 Camera</p></div>
+      <div className="box-right-top"><p>Player 2 Camera</p></div>
+      <div className="box-left-bottom"><p>Player 1 Cards</p></div>
+      <div className="box-right-bottom"><p>Player 2 Cards</p></div>      
     </div>
   );
 }
