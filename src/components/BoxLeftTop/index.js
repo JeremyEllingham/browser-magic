3492d273c7543b598092fce21e46abdd60c687e3
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-17 23:28:14 +0000

    Add webcam to box top left.

--- src/components/BoxLeftTop/index.js
+++ src/components/BoxLeftTop/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './BoxLeftTop.css'
+import Webcam from "react-webcam"
 
 const BoxLeftTop = () => { 
   return(
     <div className="container">
       <p>Player 1 Camera!</p>
+      <Webcam width={400} />
     </div>
   )
 }
