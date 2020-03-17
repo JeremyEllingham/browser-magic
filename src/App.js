0273f9ae655f46547b4fbd10d05a87cac67941b4
JeremyEllingham <jeremy@sethindustries.com.au>
2020-03-17 23:14:19 +0000

    Set up player1 camera.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css'
+import BoxTopLeft from './components/BoxLeftTop'
 
 function App() {
   return (
     <div className="grid">
-      <div className="box-left-top"><p>Player 1 Camera</p></div>
+      <div className="box-left-top"><BoxTopLeft /></div>
       <div className="box-right-top"><p>Player 2 Camera</p></div>
       <div className="box-left-bottom"><p>Player 1 Cards</p></div>
       <div className="box-right-bottom"><p>Player 2 Cards</p></div>      
---
+++ src/components/BoxLeftTop.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const BoxLeftTop = () => (<p>Player 1 Camera!</p>)
+
+export default BoxLeftTop
